4eaf11c93da829cc2d560c7687d1717cd5391c50
Katherine Huang <kat.m.huang@gmail.com>
2018-09-08 14:32:06 -0400

    Start matplotlib

--- force_detection/pressure.py
+++ force_detection/pressure.py
@@ -2,7 +2,7 @@ import serial
 from tkinter import *
 from tkinter import ttk
 import time
-import random
+import matplotlib.pyplot as plt
 
 # Windows: COM5
 # OSX: 
@@ -11,31 +11,23 @@ ser = serial.Serial(port, 9600)
 
 # GUI
 root = Tk() # main window
-frame = Frame(root, width=300, height=300)
+frame = Frame(root, width=1000, height=800)
 frame.pack()
 
+val_list = []
+
 def add_value():
-    val = ser.readline()
+    val = str(ser.readline())
+    val = val.split("=")[1] # clean the string
+    val = val.split("\\")[0]
     label_var = Label(frame, text=val)
     label_var.grid(row=0, column=0)
     root.after(1, add_value)
 
-root.after(0, add_value)  # add_letter will run as soon as the mainloop starts.
+    val_list.append(val)
+    plt.plot(val_list)
+    plt.ylabel('pressure values')
+    plt.show()
+
+root.after(0, add_value)
 root.mainloop()
-
-'''
-labelText = StringVar()
-label_var = Label(root, text="sensor data")
-label_var.grid(row=0, column=0)
-
-def update_value():
-    x = ser.readline()
-    print(x)
-    #global label_var
-    #label_var.destroy()
-    #label_var = Label(root, text=x)
-    #label_var.grid(row=0, column=0)
-
-root.after(0, update_value)
-root.mainloop()
-'''
