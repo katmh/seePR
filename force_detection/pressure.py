d53973cf75abf48d163e4ab3234af4ed9c1d82bc
Mason739 <>
2018-09-08 11:29:34 -0400

    Added __init__ file

--- force_detection/pressure.py
+++ force_detection/pressure.py
@@ -1,17 +1,20 @@
 import serial
 
+
 try:
     ser = serial.Serial("/dev/cu.usbmodem1421", 9600)
     port = '/dev/ttyACM0'
-    
+
     #msg = ard.read(ard.inWaiting())
-    
-    while True:
-        x = ser.readline()
+
+    # while True:
+    #     x = ser.readline()
+    def get_sensor_data():
+        return ser.readline()
 #print(x)
 
 except:
     print("No serial device connected!")
 
-
-    
+    def get_sensor_data():
+        return None
