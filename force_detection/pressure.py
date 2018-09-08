fe5299dbd6e0659bc5702374717c1175b76d7aba
Katherine Huang <kat.m.huang@gmail.com>
2018-09-08 14:23:41 -0400

    Show pressure value in GUI

--- force_detection/pressure.py
+++ force_detection/pressure.py
@@ -1,18 +1,41 @@
 import serial
 from tkinter import *
 from tkinter import ttk
+import time
+import random
 
-ser = serial.Serial('COM5', 9600)
+# Windows: COM5
+# OSX: 
+port = 'COM5'
+ser = serial.Serial(port, 9600)
 
-root = Tk()
+# GUI
+root = Tk() # main window
+frame = Frame(root, width=300, height=300)
+frame.pack()
+
+def add_value():
+    val = ser.readline()
+    label_var = Label(frame, text=val)
+    label_var.grid(row=0, column=0)
+    root.after(1, add_value)
+
+root.after(0, add_value)  # add_letter will run as soon as the mainloop starts.
+root.mainloop()
+
+'''
 labelText = StringVar()
 label_var = Label(root, text="sensor data")
 label_var.grid(row=0, column=0)
 
-x = ser.readline()
-print(x)
-label_var.destroy()
-label_var = Label(root, text=x)
-label_var.grid(row=0, column=0)
+def update_value():
+    x = ser.readline()
+    print(x)
+    #global label_var
+    #label_var.destroy()
+    #label_var = Label(root, text=x)
+    #label_var.grid(row=0, column=0)
 
+root.after(0, update_value)
 root.mainloop()
+'''
